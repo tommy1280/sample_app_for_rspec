52616498daf2c64c6dd5ae6520b85a4819f7688e
tommy1280 <61608564+tommy1280@users.noreply.github.com>
2020-12-23 08:13:35 +0900

    Fix: バリデーションのテストを修正

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,34 +3,36 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   describe 'validation' do
     it 'is valid with all attributes' do
-      task = FactoryBot.build(:task)
+      task = build(:task)
       expect(task).to be_valid
+      expect(task.errors).to be_empty
     end
 
     it 'is invalid without title' do
-      task_without_title = FactoryBot.build(:task, title: nil)
+      task_without_title = build(:task, title: "")
       expect(task_without_title).to be_invalid
-      expect(task_without_title.errors[:title]).to include("can't be blank")
+      expect(task_without_title.errors[:title]).to eq ["can't be blank"]
     end
 
     it 'is invalid without status' do
-      task_without_status = FactoryBot.build(:task, status: nil)
+      task_without_status = build(:task, status: nil)
       expect(task_without_status).to be_invalid
-      expect(task_without_status.errors[:status]).to include("can't be blank")
+      expect(task_without_status.errors[:status]).to eq ["can't be blank"]
 
     end
 
     it 'is invalid with a duplicate title' do
-      task = FactoryBot.create(:task)
-      task_with_duplicate_title  = FactoryBot.build(:task, content: "要件コピー", status: 'doing', deadline: Time.now.yesterday )
+      task = create(:task)
+      task_with_duplicate_title  = build(:task, title: task.title)
       expect(task_with_duplicate_title).to be_invalid
       expect(task_with_duplicate_title.errors[:title]).to eq ["has already been taken"]
     end
 
     it 'is valid with another title' do
-      task = FactoryBot.create(:task)
-      task_with_another_title = FactoryBot.build(:task, title: 'another_title')
+      task = create(:task)
+      task_with_another_title = build(:task, title: 'another_title')
       expect(task_with_another_title).to be_valid
+      expect(task_with_another_title.errors).to be_empty
     end
   end
 end
